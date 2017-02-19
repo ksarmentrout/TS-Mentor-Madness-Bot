369f9e2df650a3d2fdc0a46514dcf5cffa66d996
ksarmentrout <k.armentrout11@gmail.com>
2017-02-19 13:29:26 -0500

    changed use of url_for

--- master.py
+++ master.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, app, send_from_directory
 from functions import utils
 
 app = Flask(__name__)
@@ -17,12 +17,12 @@ def added_booking():
     # {"first_name": "Keaton", "last_name": "Armentrout", "end_time": "2/19/17 4:00 PM", "duration": "1 hour", "start_time": "2/19/17 3:00 PM", "email": "keaton.armentrout@techstarsassociates.com"}
 
     if request.method == 'POST':
-        with open(url_for('static', filename='webhook_json.txt'), 'w') as file:
+        with open('static/webhook_json.txt', 'w') as file:
             file.write(request.form)
         return
     else:
         try:
-            with open(url_for('static', filename='webhook_json.txt')) as file:
+            with open('static/webhook_json.txt') as file:
                 webhook_json = file.read()
             return render_template('webhook_display.html', webhook_json=webhook_json)
         except Exception as exc:
